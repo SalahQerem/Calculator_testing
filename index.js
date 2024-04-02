1c4823f393b0c2f652cc9bad06b7c1bc490d3af3
Salah AlDin <salahqerem@gmail.com>
2024-04-02 02:59:34 +0200

    Add task files

---
+++ index.js
@@ -0,0 +1,16 @@
+const calc = (num1, operator, num2) => {
+  switch (operator) {
+    case "+":
+      return num1 + num2;
+    case "-":
+      return num1 - num2;
+    case "*":
+      return num1 * num2;
+    case "/":
+      return num1 / num2;
+    default:
+      return "Invalid operator";
+  }
+};
+
+export default calc;

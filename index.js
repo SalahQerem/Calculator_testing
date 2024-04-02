a78f1c0bab492bca9342caa508e6a7befa9c21d0
Salah AlDin <salahqerem@gmail.com>
2024-04-02 15:21:18 +0200

    Add the calculator function

---
+++ index.js
@@ -0,0 +1,79 @@
+const calc = (...expressionTerms) => {
+  let nums = [];
+
+  let operators = [];
+
+  expressionTerms.forEach((term, index) => {
+    if (isNumber(term)) {
+      nums.push(term);
+    } else if (isOperator(term)) {
+      while (
+        operators.length !== 0 &&
+        getPrecedence(term) <= getPrecedence(operators[operators.length - 1])
+      ) {
+        let opResult = performOperation(nums, operators.pop());
+
+        nums.push(opResult);
+      }
+
+      operators.push(term);
+    } else {
+      if (index % 2) throw new Error("Invalid operator");
+      throw new Error("Invalid input type");
+    }
+  });
+
+  while (operators.length !== 0) {
+    let opResult = performOperation(nums, operators.pop());
+
+    nums.push(opResult);
+  }
+
+  return nums[0];
+};
+
+const isNumber = (term) => {
+  return typeof term === "number" && !isNaN(term);
+};
+
+const isOperator = (term) => {
+  return term === "+" || term === "-" || term === "*" || term == "/";
+};
+
+const getPrecedence = (operator) => {
+  switch (operator) {
+    case "+":
+    case "-":
+      return 1;
+    case "*":
+    case "/":
+      return 2;
+    default:
+      throw new Error("Not supported operation");
+  }
+};
+
+const performOperation = (numbers, operation) => {
+  let a = numbers.pop();
+
+  let b = numbers.pop();
+
+  switch (operation) {
+    case "+":
+      return a + b;
+    case "-":
+      return b - a;
+    case "*":
+      return a * b;
+    case "/":
+      if (a === 0) {
+        throw new Error("Division by zero");
+      }
+
+      return b / a;
+    default:
+      throw new Error("Not supported operation");
+  }
+};
+
+export default calc;
